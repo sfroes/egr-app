e2b22e0cea2540a212ab898ad016e9fd137e659a
SMC\106763 <106763@pucminas.br>
2025-11-28 09:14:40 -0300

    feat: criação dos componentes de header e footer

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+import { HeaderComponent } from './header/header.component'; // Caminho correto
+import { FooterComponent } from './footer/footer.component'; // Caminho correto
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ToastModule],
+  imports: [CommonModule, RouterOutlet, ToastModule, HeaderComponent, FooterComponent], // Mantém as importações
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
   providers: [MessageService],
